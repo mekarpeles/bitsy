e4eec0257c3e4956f4be9a062a2d3ca77071ffad
Michael E. Karpeles <michael.karpeles@gmail.com>
2012-08-15 19:43:36 -0700

    Added some documentation + examples within the docstring

--- bitsy/bitsy.py
+++ bitsy/bitsy.py
@@ -1,12 +1,13 @@
 #-*- coding: utf-8 -*-
 
 """
-    test
-    ~~~~
+    bitsy
+    ~~~~~
     This file tests and compares avg runtime performance of summing
-    the number of set bits within 1000, 16 bit 
+    the number of set bits within 1000, 16 bit numbers.
 
     :authors Mek
+    :license GPLv3
 """
 
 import math
@@ -17,17 +18,33 @@ def pad(n, bits=8):
     """Consumes the string representation of a binary number and
     prepends 0's until it's length is == to the number of bits
     specified by the bits argument
+
+    >>> pad('1001', bits=8)
+    '00001001'
     """
     return ((bits - len(n)) * "0") + n
 
+# dict lookup table (could also be built incrementally with
+# memoization, but my guess due to spacial locality it's better to get
+# it down all at once and not context switch
 lookup = dict((pad(str(b), 8), b.count('1')) for b in \
                   map(lambda n: str(bin(n)[2:]), range(0, 256)))
 
 def randbin(bits=16):
+    """Generates a str representation of a random binary 'bits' length
+    binary number.
+
+    >>> randbin(bits=8)
+    '10010111'
+    """
     return str(pad(bin(randint(0, (2**bits)-1))[2:], bits))
 
 def chunk(s, size):
-    """Yield successive n-sized chunks from str s."""
+    """Yield successive n-sized chunks from str s.
+
+    >>> chunk('10010111', 2)
+    ['1001', '0111']
+    """
     if len(s) < size:
         return [s]
     return [s[i:i+size] for i in xrange(0, len(s), size)]
@@ -35,6 +52,9 @@ def chunk(s, size):
 def setbits16(n, chunk_size=8):
     """Counts the number of set bits (the number of occurences of 1)
     within a 16-bit binary number. Uses a dict lookup table 
+
+    >>> setbits16('10010111', chunk_size=8)
+    5
     """
     return sum(map(lambda x: lookup[x], chunk(n, chunk_size)))
                    
